1a35e741c05e4590f9d00909127e7c72549374ee
Joseph Baca <josephcbaca@gmail.com>
2020-05-11 13:51:12 -0700

    Final pull from collaborative effort with Josephoine Ndungu and Caroline Lee

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ But, what if I told you there was one button that could make your decision more 
 
 Introducing [ProjectName], the one button solution for all of your food-related-decision woes.  You come to our site hungry and all you need to do is provide one thing...what are you craving?  Thai food?  Try it! Enter the cuisine and you will receive 5 recipes returned and 5 nearby restaurants returned.  Look over the selections and make your choice.  It's really that simple!
 
+User Story
+
+As a consumer
+I want an application that could give me meal suggestions
+So that I don't waste time thinking on an empty stomach
+
+User Story
+
+As a Market Analyst
+I want an application that could show me consumer trends and their choice between cooking at home or choosing to eat at a restaurant
+So that I can assist customers in both of those industries with the given data to draw in more customers
+
+https://carooflee.github.io/Six-Feet-or-Further/
--- script.js
+++ script.js
@@ -1,5 +1,98 @@
 $(document).ready(function () {
 
+    // Prompt user upon entry
+    let entryValue = localStorage.getItem("cuisineEntry")
+    let previousValue = localStorage.getItem("previousCuisine")
+
+    //If local storage = something show "Back Again" message:  We're happy to have you back again!  How did you enjoy your (last local storage entry)?  Did you end up cooking something up or venturing out to a restaurant?
+    if (entryValue === "true") {
+
+        // "Back Again" pop up
+
+
+
+        let overlayDiv = $("<div>");
+        overlayDiv.addClass("col s12 m6 overlay-1");
+        $("body").append(overlayDiv);
+
+        let popDiv = $("<div>");
+        popDiv.addClass("card blue-grey darken-1 card-over-1");
+        $(".overlay-1").append(popDiv);
+
+        let cardoverDiv = $("<div>");
+        cardoverDiv.addClass("card-content white-text pop-content");
+        cardoverDiv.text("We're happy to have you back again!  How did you enjoy your " + previousValue + " food?  Did you end up cooking something up or venturing out to a restaurant?")
+        $(".card-over-1").append(cardoverDiv);
+        
+        let cardAction = $("<div>");
+        cardAction.addClass("card-action");
+        $(".card-over-1").append(cardAction);
+
+        let popA1 = $("<a>");
+
+        popA1.addClass("col s12");
+        popA1.text("I Cooked!");
+        popA1.attr("id", "popup-1");
+        $(".card-action").append(popA1);
+
+        let popA2 = $("<a>");
+        popA2.addClass("col s12");
+        popA2.text("I Grabbed Something!");
+
+        popA2.attr("id", "popup-2");
+        $(".card-action").append(popA2);
+
+    } else {
+
+        // "First Time" pop up
+
+        let overlayDiv = $("<div>");
+        overlayDiv.addClass("col s12 m6 overlay-1");
+        $("body").append(overlayDiv);
+
+        let popDiv = $("<div>");
+        popDiv.addClass("card blue-grey darken-1 card-over-1");
+        $(".overlay-1").append(popDiv);
+
+        let cardoverDiv = $("<div>");
+        cardoverDiv.addClass("card-content white-text pop-content");
+        cardoverDiv.text("Hungry?  We're here to help! Since this is your first time visiting our site, simply type in what you are craving (i.e. Italian), click search and let us do the rest!")
+        $(".card-over-1").append(cardoverDiv);
+        
+        let cardAction = $("<div>");
+        cardAction.addClass("card-action");
+        $(".card-over-1").append(cardAction);
+
+        let popA3 = $("<a>");
+        popA3.text("Click to start!");
+        popA3.attr("id", "popup-start");
+        $(".card-action").append(popA3);
+
+    }
+
+
+    // Adds 0 value to localStorage
+
+    $("#popup-start").on("click", function () {
+        $(".overlay-1").hide();
+        localStorage.setItem("restaurant", 0)
+        localStorage.setItem("recipe", 0)
+    });
+    // Metrics:  Adds to either Restaurant Count or Recipe Count
+    $("#popup-1").on("click", function () {
+        $(".overlay-1").hide();
+        let addRestaurant = parseInt(localStorage.getItem("restaurant"));
+        let addedRes = addRestaurant + 1;
+        localStorage.setItem("restaurant", addedRes);
+    });
+    $("#popup-2").on("click", function () {
+        $(".overlay-1").hide();
+        let addRecipe = parseInt(localStorage.getItem("recipe"));
+        let addedRec = addRecipe + 1;
+        localStorage.setItem("recipe", addedRec);
+    });
+
+
     // Input Group
     let headerDiv = $("<div>");
     let formGroupDiv = $("<div>");
@@ -26,25 +119,31 @@ $(document).ready(function () {
 
     //Click fades Input Group out
     $("#search-button").on("click", function () {
-        $(".searchDiv").fadeOut();
-        $("#foodType").fadeOut();
+        $(".searchDiv").hide();
+
     });
 
     // Same click fades Card contents in
     $("#search-button").on("click", function () {
         let cuisineValue = $("#foodType").val().trim();
-
+        localStorage.setItem("cuisineEntry", true);
+        localStorage.setItem("previousCuisine", cuisineValue)
         searchRestaurant(cuisineValue);
     });
 
+    // Segue between search and results(This helps with aesthetic due to timing delay with two AJAX calls)
+    $(".hungry").text("Are you ready to eat?").hide();
+
+    $("#search-button").on("click", function () {
+        $(".hungry").fadeIn("slow");
+    });
 
     function searchRestaurant(cuisine) {
 
+        // API Restaurants Zomato.com
         let APIKey = "34e52954cf804b1932d1e06de180b698";
-        let APIKey2 = "6b993b882bmsha7daac7eff39d3cp1825fcjsn0ba77ad0c74a";
-        // URL to query the database
         let queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=279&entity_type=city&q=" + cuisine + "&count=100&sort=rating&order=desc&apikey=" + APIKey;
-        // let queryURL2 = "https://tasty.p.rapidapi.com/recipes/list?tags=under_30_minutes&q=" + cuisine + "&from=0&sizes=5&apikey=" + APIKey2;
+        // API Recipes Tasty via rapidapi.com
         let settings = {
             "async": true,
             "crossDomain": true,
@@ -55,20 +154,30 @@ $(document).ready(function () {
                 "x-rapidapi-key": "6b993b882bmsha7daac7eff39d3cp1825fcjsn0ba77ad0c74a"
             }
         }
-        // We then created an AJAX call
+
+        // AJAX call to first call Recipes JSON data
         $.ajax(settings).then(function (response2) {
 
 
+            $(".hungry").fadeOut();
+            //AJAX call to then call data gathered by Recipe AJAX call to async full call of all data
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (response) {
 
+                $("#row-1").text("Restaurants!").addClass("header");
+                $("#row-2").text("Recipes!").addClass("header");
+
                 // for loop cycles through Card creation below
                 for (let i = 0; i < 4; i++) {
-                    // Card
+                  
+                    // Restaurant Cards
+
+         
+
                     let colm = $("<div>");
-                    colm.addClass("card resrow-" + [i]);
+                    colm.addClass("card card-custom resrow-" + [i]);
                     $("#restRow").append(colm);
 
                     let restDiv = $("<h3>");
@@ -89,44 +198,43 @@ $(document).ready(function () {
                 };
 
                 for (let i = 0; i < 4; i++) {
-                    // Card
+
+                    // Recipe Cards
+
+                    let food = response2.results;
                     let colm2 = $("<div>");
-                    colm2.addClass("card recrow-" + [i]);
+                    colm2.addClass("card card-custom recrow-" + [i]);
                     $("#recRow").append(colm2);
 
                     let restDiv = $("<h3>");
                     restDiv.text(response2.results[i].name)
                     $(".recrow-" + [i]).append(restDiv);
 
-                    let addP = $("<p>");
-                    addP.text("Click Here for Recipe!");
-                    $(".recrow-" + [i]).append(addP);
 
-                };
+                    let addRecA = $("<a>");
+                    addRecA.text("Click Here for Recipe!")
+                    $(".recrow-" + [i]).append(addRecA);
+                    // From A tag above on click empty body and show Recipe list from user's selection
+                    addRecA.on("click", function () {
 
-                // let foods = response.results;
-                // $.each(foods, function (index, food) {
-                //     let foodTitle = $("<button>").text(food.name);
-                //     $(".display-food").append(foodTitle);
-                //     foodTitle.on("click", function () {
-                //         $(".display-food").empty();
-                //         let foodDiv = $("<div>").addClass("card");
-                //         let foodImage = $("<img>").attr("src", food.thumbnail_url).width("150px").height("150px");
-                //         let recipeArr = food.instructions;
-                //         foodDiv.append(foodImage);
-                //         let instructionsDiv = $("<div>").addClass("container");
-                //         for (let i = 0; i < recipeArr.length; i++) {
-                //             let foodRecipe = $("<li>").text(recipeArr[i].display_text);
-                //             console.log(recipeArr[i].display_text);
-                //             instructionsDiv.append(foodRecipe);
-                //         }
-                //         foodDiv.append(instructionsDiv);
-                //         $(".display-food").append(foodDiv);
-                //     });
-                // });
+                        $("body").empty();
+                        let foodDiv = $("<div>").addClass("card recipe");
+                        let foodImage = $("<img>").attr("src", food[i].thumbnail_url).width("150px").height("150px");
+                        let recipeArr = food[i].instructions;
+                        foodDiv.append(foodImage);
+                        let instructionsDiv = $("<div>").addClass("container");
+                        for (let i = 0; i < recipeArr.length; i++) {
+                            let foodRecipe = $("<li>").text(recipeArr[i].display_text);
+                            instructionsDiv.append(foodRecipe);
+                        }
+                        foodDiv.append(instructionsDiv);
 
+                    $("body").append(foodDiv);
+                    
 
+                })};
             });
         });
     };
+
 });
